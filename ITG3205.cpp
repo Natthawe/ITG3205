21a147227c4402d64504c10c86579171875da599
ikiselev <vanwin@yandex.ru>
2013-12-03 20:10:07 +0400

    ITG3205 read gyro data

--- ITG3205.cpp
+++ ITG3205.cpp
@@ -1,2 +1,50 @@
+#include <Wire.h>
 #include "ITG3205.h"
 
+
+void ITG3205::initGyro() {
+    WriteByte(ITG3200_Address, PWR_MGM, 0x00);
+    WriteByte(ITG3200_Address, SMPLRT_DIV, 0x07); //Fsample = 1kHz / (7 + 1) = 125Hz, or 8ms per sample
+    //WriteByte(ITG3200_Address, DLPF_FS, 0x1E); // +/- 2000 dgrs/sec, 1KHz, Low Pass Filter Bandwidth: 5Hz
+    WriteByte(ITG3200_Address, DLPF_FS, 0x1B); // +/- 2000 dgrs/sec, 1KHz, Low Pass Filter Bandwidth: 42Hzh
+    WriteByte(ITG3200_Address, INT_CFG, 0x00);
+}
+
+void ITG3205::GyroRead() {
+    Wire.beginTransmission(ITG3200_Address);
+    Wire.write(0x1B);
+    Wire.endTransmission();
+
+    Wire.beginTransmission(ITG3200_Address);
+    Wire.requestFrom(ITG3200_Address, 8);    // request 8 bytes from ITG3200
+
+    int i = 0;
+    byte buff[8];
+    while(Wire.available())
+    {
+        buff[i] = Wire.read();
+        i++;
+    }
+
+    Wire.endTransmission();
+
+    g.t = (buff[0] << 8) | buff[1]; // temperature
+    g.x = ((buff[2] << 8) | buff[3]);
+    g.y = ((buff[4] << 8) | buff[5]);
+    g.z = ((buff[6] << 8) | buff[7]);
+}
+
+
+byte ITG3205::WriteByte(byte i2c_address, byte address, byte data) {
+    Wire.beginTransmission(i2c_address);
+    Wire.write(address);
+    Wire.write(data);
+    byte result = Wire.endTransmission();
+
+    //do some error checking
+    if (result > 0) {
+        Serial.print("I2C Write PROBLEM..... Result code is ");
+        Serial.println(result);
+    }
+    return result;
+}
--- ITG3205.h
+++ ITG3205.h
@@ -1,9 +1,43 @@
+#include "Arduino.h"
+
 #ifndef __ITG3205_H_
 #define __ITG3205_H_
 
 
 class ITG3205 {
 
+#define ITG3200_Address 0x68
+#define PWR_MGM 0x3E
+#define SMPLRT_DIV 0x15
+#define DLPF_FS 0x16
+#define INT_CFG 0x17
+
+
+#define TEMP_OUT_H 0x1B
+#define TEMP_OUT_L 0x1C
+
+#define GYRO_XOUT_H 0x1D
+#define GYRO_XOUT_L 0x1E
+
+#define GYRO_YOUT_H 0x1F
+#define GYRO_YOUT_L 0x20
+
+#define GYRO_ZOUT_H 0x21
+#define GYRO_ZOUT_L 0x22
+
+public:
+    typedef struct vector
+    {
+        int x, y, z, t;
+    } vector;
+
+    vector g; // gyro angular velocity readings
+
+    void initGyro();
+    void GyroRead();
+
+
+    byte WriteByte(byte i2c_address, byte address, byte data);
 };
 
 
