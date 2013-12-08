dce00a758971f2a40b0ec96acace4f1748633b96
ikiselev <vanwin@yandex.ru>
2013-12-08 21:30:11 +0400

    Read gyro calibrated and raw

--- ITG3205.cpp
+++ ITG3205.cpp
@@ -3,37 +3,70 @@
 
 
 void ITG3205::initGyro() {
-    WriteByte(ITG3200_Address, PWR_MGM, 0x00);
-    WriteByte(ITG3200_Address, SMPLRT_DIV, 0x07); //Fsample = 1kHz / (7 + 1) = 125Hz, or 8ms per sample
-    //WriteByte(ITG3200_Address, DLPF_FS, 0x1E); // +/- 2000 dgrs/sec, 1KHz, Low Pass Filter Bandwidth: 5Hz
-    WriteByte(ITG3200_Address, DLPF_FS, 0x1B); // +/- 2000 dgrs/sec, 1KHz, Low Pass Filter Bandwidth: 42Hzh
-    WriteByte(ITG3200_Address, INT_CFG, 0x00);
+    WriteByte(address, PWR_MGM, 0x00);
+    WriteByte(address, SMPLRT_DIV, 0x07); //Fsample = 1kHz / (7 + 1) = 125Hz, or 8ms per sample
+    //WriteByte(address, DLPF_FS, 0x1E); // +/- 2000 dgrs/sec, 1KHz, Low Pass Filter Bandwidth: 5Hz
+    WriteByte(address, DLPF_FS, 0x1B); // +/- 2000 dgrs/sec, 1KHz, Low Pass Filter Bandwidth: 42Hzh
+    WriteByte(address, INT_CFG, 0x00);
+}
+
+void ITG3205::calibrate() {
+    int x, y, z, i, count = 128;
+    int xSum = 0, ySum = 0, zSum = 0;
+
+    for(i = 0; i < count; i++) {
+        delay(5);
+        readGyroRaw(&x, &y, &z);
+        xSum += x;
+        ySum += y;
+        zSum += z;
+    }
+
+    offset[0] = -xSum / count;
+    offset[1] = -ySum / count;
+    offset[2] = -zSum / count;
+}
+
+void ITG3205::readGyroRaw(int *x, int *y, int *z) {
+    Wire.beginTransmission(address);
+    Wire.write(GYRO_XOUT_H);
+    Wire.endTransmission();
+
+    Wire.requestFrom(address, (byte)6);
+
+    *x = readShortI2C();
+    *y = readShortI2C();
+    *z = readShortI2C();
 }
 
 void ITG3205::GyroRead() {
-    Wire.beginTransmission(ITG3200_Address);
-    Wire.write(0x1B);
+    Wire.beginTransmission(address);
+    Wire.write(GYRO_XOUT_H);
     Wire.endTransmission();
 
-    Wire.beginTransmission(ITG3200_Address);
-    Wire.requestFrom(ITG3200_Address, 8);    // request 8 bytes from ITG3200
+    Wire.requestFrom(address, (byte)6);    // request 8 bytes from ITG3200
 
-    int i = 0;
-    byte buff[8];
-    while(Wire.available())
-    {
-        buff[i] = Wire.read();
-        i++;
-    }
+    //g.t = (buff[0] << 8) | buff[1]; // temperature
+    g.x = readShortI2C() + offset[0];
+    g.y = readShortI2C() + offset[1];
+    g.z = readShortI2C() + offset[2];
 
     Wire.endTransmission();
 
-    g.t = (buff[0] << 8) | buff[1]; // temperature
-    g.x = ((buff[2] << 8) | buff[3]);
-    g.y = ((buff[4] << 8) | buff[5]);
-    g.z = ((buff[6] << 8) | buff[7]);
+    g.x /= LSB_DEG;
+    g.y /= LSB_DEG;
+    g.z /= LSB_DEG;
+
 }
 
+int ITG3205::readShortI2C() {
+    return (signed short)readWordI2C();
+}
+
+
+int ITG3205::readWordI2C() {
+    return (Wire.read() << 8) | Wire.read();
+}
 
 byte ITG3205::WriteByte(byte i2c_address, byte address, byte data) {
     Wire.beginTransmission(i2c_address);
--- ITG3205.h
+++ ITG3205.h
@@ -12,6 +12,8 @@ class ITG3205 {
 #define DLPF_FS 0x16
 #define INT_CFG 0x17
 
+#define LSB_DEG 14.375
+
 
 #define TEMP_OUT_H 0x1B
 #define TEMP_OUT_L 0x1C
@@ -25,7 +27,9 @@ class ITG3205 {
 #define GYRO_ZOUT_H 0x21
 #define GYRO_ZOUT_L 0x22
 
+    byte address;
 public:
+    ITG3205(): address(ITG3200_Address) {}
     typedef struct vector
     {
         int x, y, z, t;
@@ -33,11 +37,20 @@ public:
 
     vector g; // gyro angular velocity readings
 
+    int offset[3];
+
+    void calibrate();
     void initGyro();
     void GyroRead();
 
 
     byte WriteByte(byte i2c_address, byte address, byte data);
+
+    void readGyroRaw(int *x, int *y, int *z);
+
+    int readShortI2C();
+
+    int readWordI2C();
 };
 
 
